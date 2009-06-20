267e97554ebde29b05b1326dc73c3a87a5c86ccc
Mike <mike@leerbot.(none)>
2009-06-19 19:45:18 -0700

    Removed weight property from prop. Unused so far.

--- engine/class/prop.class.rb
+++ engine/class/prop.class.rb
@@ -3,7 +3,7 @@ class Prop
   include May_Have_Name
   include Has_Events
 
-  attr_accessor :id, :name, :description, :location, :weight, :size, :portable, :contains, :opens_with, :opened, \
+  attr_accessor :id, :name, :description, :location, :size, :portable, :contains, :opens_with, :opened, \
     :get_with, :build_with, :text, :attack_strength, :events, :visible, :traits
 
   def initialize
