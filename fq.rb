8271d1f9cb3006216fe913a08cd149404ff859b3
Mike <mcantelon@gmail.com>
2009-08-03 15:34:55 -0700

    Moved requires into separate include.

--- fq.rb
+++ fq.rb
@@ -1,11 +1,11 @@
-require 'yaml'
-
 app_base_path = File.expand_path(File.dirname(__FILE__))
 path          = ARGV[1] ? ARGV[1] + '/' : 'game/'
 
 if not File.directory? path
   path = app_base_path + '/' + path
 end
+
+require 'engine/requirements.rb'
 
 config = File.open("#{path}config.yaml", 'r') { |f| YAML::load(f.read) }
 
@@ -15,23 +15,6 @@ Shoes.app(
   :height    => config['height'],
   :resizable => config['resizable']
 ) {
-
-  require 'engine/module/handles_yaml_files.module.rb'
-  require 'engine/module/parses_commands.module.rb'
-  require 'engine/module/uses_weapons.module.rb'
-  require 'engine/module/may_have_name.rb'
-  require 'engine/module/has_events.rb'
-  require 'engine/module/has_traits.rb'
-
-  require 'engine/class/game.class.rb'
-  require 'engine/class/game_component.class.rb'
-  require 'engine/class/player.class.rb'
-  require 'engine/class/character.class.rb'
-  require 'engine/class/locations.class.rb'
-  require 'engine/class/door.class.rb'
-  require 'engine/class/prop.class.rb'
-  require 'engine/class/cli.class.rb'
-  require 'engine/class/command.class.rb'
 
   # Initialize image display area
   @image_stack = stack :background => white,
