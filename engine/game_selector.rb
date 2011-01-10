4c014dd62781a29d2faf66ada9c7ccca0c23a1fc
Mike Cantelon <mcantelon@gmail.com>
2011-01-09 20:56:59 -0800

    Fixed bug with starting preset game and moves games into 'games' directory.

--- engine/game_selector.rb
+++ engine/game_selector.rb
@@ -2,30 +2,45 @@ def game_selector(app, app_base_path)
 
   game_directories = game_selector_directories(app_base_path)
 
-  if game_directories.size == 1
+  stack do
 
-    game_selector_launch(app, app_base_path, game_directories.first)
+    if game_directories.size == 1
 
-  else
+      message = "Loading " + game_directories.first
+      game = game_directories.first
 
-    text       = para "Choose game:"
-    text.align = 'center'
+      #text       = para ("Loading " + game_directories.first)
+      #text.align = 'center'
 
-    game_select = list_box :items => game_directories
+      #btn = button 'OK' do
+      #  game_selector_launch(app, app_base_path, game_directories.first)
+      #end
 
-    btn = button 'OK' do
-      if game_select.text()
-        game_selector_launch(app, app_base_path, game_select.text())
-      end
+    else
+
+      message = "Choose game:"
+
     end
 
-  end
+    text       = para message
+    text.align = 'center'
 
+    if !game
+      game_select = list_box :items => game_directories
+    end
+
+    btn = button 'OK' do
+      if game || game_select.text()
+        load_game = game ? game : game_select.text()
+        game_selector_launch(app, app_base_path, load_game)
+      end
+    end
+  end
 end
 
 def game_selector_launch(app, app_base_path, game_directory)
 
-  game_path = app_base_path + '/' + game_directory + '/'
+  game_path = app_base_path + '/games/' + game_directory + '/'
   config = File.open("#{game_path}config.yaml", 'r') { |f| YAML::load(f.read) }
   main(app, app_base_path, game_path, config)
 
@@ -34,10 +49,11 @@ end
 def game_selector_directories(app_base_path)
 
   game_directories = []
+  game_directory = app_base_path + '/games'
 
   # Scan each directory in the application directory for a config.yaml file
-  Dir.entries(app_base_path).each do |entry|
-    path = app_base_path + '/' + entry
+  Dir.entries(game_directory).each do |entry|
+    path = game_directory + '/' + entry
     if path != '.' && path != '..' && FileTest.directory?(path)
       Dir.entries(path).each do |child_entry|
         if child_entry == 'config.yaml'
