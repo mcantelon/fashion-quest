0753f2f198b293baad2f362abfd0ad231b4f3a6d
Mike Cantelon <mcantelon@gmail.com>
2011-01-10 01:08:01 -0800

    Minor tweak.

--- engine/module/handles_yaml_files.module.rb
+++ engine/module/handles_yaml_files.module.rb
@@ -2,7 +2,7 @@ module Handles_YAML_Files
 
   def load_yaml_file(file_path)
     if FileTest.exists?(file_path)
-      File.open(file_path, 'r') { |f| YAML::load(f.read) }
+      YAML::load(File.open(file_path))
     end
   end
 
